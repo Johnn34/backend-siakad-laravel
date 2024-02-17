ed1aa95b1c9ec4e86fe802cc676157987b746922
johnN34 <yohanesnancu95@gmail.com>
2024-02-17 11:07:54 +0700

    crud users

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory(10)->create();
+
+        User::create([
+            'name' => 'John Admin',
+            'email' => 'john@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('123456'),
+            'roles'=>'admin',
+        ]);
+    }
+}
